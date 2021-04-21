3b4e1137f9d93e97cc69caf64551b4fbb4e35f86
Oleksii Korniienko <60704619+olkornii@users.noreply.github.com>
2021-04-21 10:08:06 +0200

    Update build_local.sh

--- scripts/build_local.sh
+++ scripts/build_local.sh
@@ -7,8 +7,6 @@ set -e
 
 [ -x "$(command -v kind)" ] && [[ "$(kubectl config current-context)" =~ ^kind-? ]] && KIND=1 NO_MINIKUBE=1
 
-kubectl config view
-
 if [ -z "$NO_MINIKUBE" ]; then
   pgrep -f "[m]inikube" >/dev/null || minikube start --kubernetes-version="v1.16.4" --extra-config=apiserver.v=4 || { echo 'Cannot start minikube.'; exit 1; }
   eval "$(minikube docker-env)" || { echo 'Cannot switch to minikube docker'; exit 1; }
@@ -16,16 +14,3 @@ if [ -z "$NO_MINIKUBE" ]; then
 fi
 
 kubectl config view
-
-# kubectl get serviceaccount
-
-# kubectl -n kube-system describe secret $(sudo kubectl -n kube-system get secret | (grep default || echo "$_") | awk '{print $1}') | grep token: | awk '{print $2}'
-
-# docker build -f e2e.Dockerfile -t quay.io/operator-framework/olm:local -t quay.io/operator-framework/olm-e2e:local ./bin
-# docker build -f test/e2e/hang.Dockerfile -t hang:10 ./bin
-
-# if [ -n "$KIND" ]; then
-#   kind load docker-image quay.io/operator-framework/olm:local
-#   kind load docker-image quay.io/operator-framework/olm-e2e:local
-#   kind load docker-image hang:10
-# fi
