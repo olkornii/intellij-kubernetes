7f8a2aeb56585af3c46deddaa65bf691a3917486
Oleksii Korniienko <olkornii@redhat.com>
2021-05-26 13:41:47 +0200

    small fixes
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- src/it/java/org/jboss/tools/intellij/kubernetes/BasicTests.java
+++ src/it/java/org/jboss/tools/intellij/kubernetes/BasicTests.java
@@ -58,9 +58,9 @@ public class BasicTests {
             toolWindowsPaneFixture.stripeButton("Kubernetes").click();
             final KubernetesToolsFixture kubernetesToolsFixture = robot.find(KubernetesToolsFixture.class);
             waitFor(Duration.ofSeconds(15), Duration.ofSeconds(1), "Kubernetes Tree View is not available.", () -> isKubernetesViewTreeAvailable(kubernetesToolsFixture));
-            String clusterText = kubernetesToolsFixture.kubernetesViewTree().findAllText().get(1).getText();
+            String clusterText = kubernetesToolsFixture.kubernetesViewTree().findAllText().get(0).getText();
             assertTrue(clusterText.contains("minikube"));
-            kubernetesToolsFixture.kubernetesViewTree().findAllText().get(1).doubleClick(MouseButton.LEFT_BUTTON);
+            kubernetesToolsFixture.kubernetesViewTree().findAllText().get(0).doubleClick(MouseButton.LEFT_BUTTON);
             kubernetesToolsFixture.kubernetesViewTree().findText("Namespaces").doubleClick(MouseButton.LEFT_BUTTON);
             waitFor(Duration.ofSeconds(15), Duration.ofSeconds(1), "Namespace is not available.", () -> isNamespaceLoaded(kubernetesToolsFixture));
 //            String namespaces = kubernetesToolsFixture.kubernetesViewTree().findAllText().get(1).getText();
