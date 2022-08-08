d8f450bb3ef837e17dbce813713546f9bb7a718b
Oleksii Korniienko <olkornii@redhat.com>
2022-08-08 13:34:32 +0200

    try
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- src/it/java/org/jboss/tools/intellij/kubernetes/BasicTests.java
+++ src/it/java/org/jboss/tools/intellij/kubernetes/BasicTests.java
@@ -65,6 +65,8 @@ public class BasicTests {
         KubernetesToolsFixture kubernetesToolsFixture = robot.find(KubernetesToolsFixture.class);
         kubernetesViewTree = kubernetesToolsFixture.getKubernetesViewTree();
         waitFor(Duration.ofSeconds(15), Duration.ofSeconds(1), "Kubernetes Tree View is not available.", BasicTests::isKubernetesViewTreeAvailable);
+
+        EditResourceTest.editResource(robot, kubernetesViewTree);
     }
 
     @AfterAll
@@ -77,17 +79,17 @@ public class BasicTests {
         step("New Empty Project", () -> ClusterConnectedTest.checkClusterConnected(kubernetesViewTree));
     }
 
-    @Test
+//    @Test
     public void openResourceEditor() {
         step("open Resource Editor", () -> OpenResourceEditorTest.checkResourceEditor(robot, kubernetesViewTree));
     }
 
-     @Test
+//     @Test
     public void editResource() {
         step("edit Resource", () -> EditResourceTest.editResource(robot, kubernetesViewTree));
     }
 
-     @Test
+//     @Test
     public void createResourceByEdit() {
         step("create Resource", () -> CreateResourceByEditTest.createResourceByEdit(robot, kubernetesViewTree));
 
